d6051f18a633fb608da49c250f8fc9c4d8f60011
Kwanusu <kwanusujoseph@gmail.com>
2024-07-15 16:48:00 +0300

    updated the merge

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-<<<<<<< HEAD
 import './interceptors/axios';
 import '@popperjs/core';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -14,11 +13,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   
       <App />
 
-=======
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
->>>>>>> 411d689f3d03729cae408f73864a83979a0c9a42
   </React.StrictMode>,
 )
